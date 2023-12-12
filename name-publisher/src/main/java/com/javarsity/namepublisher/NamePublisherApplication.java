88d635f8f9c39ba9edb07fc1e51ea1f2ef1cbaf7
Rahul A Shrivastava <rahul.a.shrivastava@tietoevry.com>
2023-12-12 23:58:29 +0530

    Initial commit.

---
+++ name-publisher/src/main/java/com/javarsity/namepublisher/NamePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.javarsity.namepublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamePublisherApplication.class, args);
+	}
+
+}
---
+++ name-publisher/src/test/java/com/javarsity/namepublisher/NamePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarsity.namepublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamePublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
